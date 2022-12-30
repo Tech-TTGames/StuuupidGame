b2752e815cdaf903fd6d14501f0ab86da21b4ea3
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2022-12-30 21:31:01 +0100

    Named window.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,7 +58,7 @@
             this.Controls.Add(this.pictureBox1);
             this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "THE SNEK GAME";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
